52f4d0ba49477739a5fc791bea28ab6a94b24c44
Dhruv Amin <Dhruv@Dhruvs-MacBook-Pro.local>
2015-04-29 01:25:23 -0700

    Added simple Uniform Cost Model calculation

--- src/edu/stanford/cs276/UniformCostModel.java
+++ src/edu/stanford/cs276/UniformCostModel.java
@@ -4,9 +4,15 @@ public class UniformCostModel implements EditCostModel {
 	
 	@Override
 	public double editProbability(String original, String R, int distance) {
-		return 0.5;
 		/*
-		 * Your code here
+		 * Your code here - NOTE FROM DHRUV, added code
 		 */
+		if (original.equals(R)) return EQUAL_PROBABILITY;
+		
+		return Math.pow(EDIT_PROBABILITY, distance);
 	}
+	
+	//we should play with these numbers
+	private static final double EQUAL_PROBABILITY = 0.95;
+	private static final double EDIT_PROBABILITY = 0.1;
 }
