70b386977bf6c36a966d08dfc4a97034604d58c7
Dhruv Amin <Dhruv@Dhruvs-MacBook-Pro.local>
2015-05-02 11:43:37 -0700

    removed model files so that its not a huge commit

--- src/edu/stanford/cs276/RunCorrector.java
+++ src/edu/stanford/cs276/RunCorrector.java
@@ -3,14 +3,12 @@ package edu.stanford.cs276;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
 import edu.stanford.cs276.util.Candidate;
 import edu.stanford.cs276.util.Pair;
+
 
 public class RunCorrector {
 
