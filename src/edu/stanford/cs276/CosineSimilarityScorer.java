8f83f98a3e54c0317b77a791aac992087ea5ac83
Dhruv Amin <dhruvamin@dhruvamin-macbookair.roam.corp.google.com>
2015-05-17 01:54:39 -0700

    Add check for term existing in doc

--- src/edu/stanford/cs276/CosineSimilarityScorer.java
+++ src/edu/stanford/cs276/CosineSimilarityScorer.java
@@ -82,7 +82,7 @@ public class CosineSimilarityScorer extends AScorer {
 						break;
 				}
 				//TODO decide if we should do sublinear scaling on document term frequencies
-				termScore += weight * termFreq.get(term); //weight * subLinearScale(termFreq.get(term))
+				if (termFreq.containsKey(term)) termScore += weight * termFreq.get(term); //weight * subLinearScale(termFreq.get(term))
 			}
 			documentVector.put(term, termScore);
 		}
