cf457e3ada71b3b5b501a2195371e81fc6b58aa8
jmwebb <jmwebb@stanford.edu>
2015-05-17 12:39:32 -0700

    Small Window probs

--- src/edu/stanford/cs276/SmallestWindowScorer.java
+++ src/edu/stanford/cs276/SmallestWindowScorer.java
@@ -22,28 +22,32 @@ public class SmallestWindowScorer extends CosineSimilarityScorer {
 	double boostmod = -1;
 
 	//////////////////////////////
-	
+
 	public SmallestWindowScorer(Map<String, Double> idfs) {
 		super(idfs);
 	}
-	
+
 	//find the smallest window and set the params
 	public void handleSmallestWindow(Document d, Query q) {
 		/*
 		 * @//TODO : Your code here
 		 */
 		double curSmallestWindow = Double.POSITIVE_INFINITY;
-		
+
 		//TODO FIll this out for each type 
 		curSmallestWindow = checkWindow(q, d, d.title, curSmallestWindow, false);
-		for (String anchor : d.anchors.keySet()) {
-			curSmallestWindow = checkWindow(q, d, anchor, curSmallestWindow, false);
+		if (d.anchors != null) {
+			for (String anchor : d.anchors.keySet()) {
+				curSmallestWindow = checkWindow(q, d, anchor, curSmallestWindow, false);
+			}
 		}
-		for (String header : d.headers) {
-			curSmallestWindow = checkWindow(q, d, header, curSmallestWindow, false);
+		if (d.headers != null) {
+			for (String header : d.headers) {
+				curSmallestWindow = checkWindow(q, d, header, curSmallestWindow, false);
+			}
 		}
 		curSmallestWindow = checkWindow(q, d, "", curSmallestWindow, true); //body hits
-		
+
 		if (curSmallestWindow == Double.POSITIVE_INFINITY) {
 			boostmod = 1 / B;
 		} else {
@@ -58,7 +62,7 @@ public class SmallestWindowScorer extends CosineSimilarityScorer {
 		 * @//TODO : Your code here
 		 */
 		double window;
-		
+
 		//turn a string into a hit list of query terms and positions, or just use body_hits
 		Map<String, List<Integer>> hits;
 		if (isBodyField) {
@@ -66,34 +70,34 @@ public class SmallestWindowScorer extends CosineSimilarityScorer {
 		} else {
 			hits = turnIntoHits(docstr, q);
 		}
-		
+
 		//make sure the query terms are all in the hit list. If not, smallest window stays the same
 		if (queryNotFullyContained(q, hits)) {
 			return curSmallestWindow;
 		}
-		
+
 		window = getSmallestWindowFromHits(hits);
-		
+
 		return (window < curSmallestWindow) ? window : curSmallestWindow;
 	}
-	
+
 	//use smallest window algorithm to get the size of the smallest window that contains all terms in hits
 	private double getSmallestWindowFromHits(Map<String, List<Integer>> hits) {
 		double smallestWindow = Double.POSITIVE_INFINITY;
-		
+
 		SortedSet<String> queryTerms = new TreeSet<String>(hits.keySet());
-	
+
 		List<List<Integer>> termPositions = new ArrayList<List<Integer>>();
 		for (String term : queryTerms) {
 			List<Integer> positions = new ArrayList<Integer>(hits.get(term));
 			termPositions.add(positions);
 		}
-		
+
 		List<Integer> curPositions = new ArrayList<Integer>();
 		for (List<Integer> positions : termPositions) {
 			curPositions.add(positions.remove(0));
 		}
-		
+
 		while (true) {
 			double window = findWindow(curPositions);
 			if (window < smallestWindow) smallestWindow = window;
@@ -101,10 +105,10 @@ public class SmallestWindowScorer extends CosineSimilarityScorer {
 			int minIndex = curPositions.indexOf(Collections.min(curPositions));
 			curPositions.set(minIndex, termPositions.get(minIndex).remove(0));
 		}
-		
+
 		return smallestWindow;
 	}
-	
+
 	//look through the various term position lists. If any are empty, return false. Otherwise, return true
 	private boolean positionsEmpty(List<List<Integer>> termPositions) {
 		for (List<Integer> positions : termPositions) {
@@ -112,21 +116,21 @@ public class SmallestWindowScorer extends CosineSimilarityScorer {
 		}
 		return false;
 	}
-	
+
 	//return the window size of this list of positions (max - min)
 	private double findWindow(List<Integer> curPositions) {
 		if (curPositions.size() <= 1) return 1;
-		
+
 		double min = curPositions.get(0);
 		double max = curPositions.get(0);
 		for (Integer pos : curPositions) {
 			if (pos < min) min = pos;
 			if (pos > max) max = pos;
 		}
-		
+
 		return max - min;
 	}
-	
+
 	//checks to make sure every word in the query is contained in the doc string
 	private boolean queryNotFullyContained(Query q, Map<String, List<Integer>> hits) {
 		for (String term : q.queryWords) {
@@ -134,7 +138,7 @@ public class SmallestWindowScorer extends CosineSimilarityScorer {
 		}
 		return false;
 	}
-	
+
 	//turns any string into a Map of position hits like body_hits. Only contains the query terms
 	private Map<String, List<Integer>> turnIntoHits(String docString, Query q) {
 		Map<String, List<Integer>> hits = new HashMap<String, List<Integer>>();
@@ -156,14 +160,14 @@ public class SmallestWindowScorer extends CosineSimilarityScorer {
 		}
 		return hits;
 	}
-	
-	
+
+
 	private double findWindowInString(Query q, String docstr) {
 		int window = 0;
-		
+
 		return window;
 	}
-	
+
 	//for each term in the document vector, boost it by the appropriate boost amount based on the found
 	//parameters of B and boostmod
 	private void boostDocumentScore(Map<String, Double> documentVector) {
@@ -171,23 +175,23 @@ public class SmallestWindowScorer extends CosineSimilarityScorer {
 			documentVector.put(term, documentVector.get(term) * B * boostmod);
 		}
 	}
-	
+
 	@Override
 	public double getSimScore(Document d, Query q) {
 		Map<String,Map<String, Double>> tfs = this.getDocTermFreqs(d,q);
-		
+
 		this.normalizeTFs(tfs, d, q);
-		
+
 		Map<String,Double> tfQuery = getQueryFreqs(q);
-		
+
 		Map<String, Double> documentVector = getDocumentVector(tfQuery, tfs);
-		
+
 		normalizeQFs(tfQuery);
-		
+
 		handleSmallestWindow(d, q);
-		
+
 		boostDocumentScore(documentVector);
-		
+
 		return getNetScore(documentVector, tfQuery);
 	}
 
