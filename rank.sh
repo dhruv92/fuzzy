5c217ff45750b0c9164b37fea9e3f6a9bc8091b7
Dhruv Amin <dhruvamin@dhruvamin-macbookair.roam.corp.google.com>
2015-05-27 23:14:24 -0700

    Switched out fuzzy for pa4

--- rank.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-# ./rank.sh <signal_data_path> taskType
-java -Xmx1024m -cp bin/ edu.stanford.cs276.Rank $1 $2
--- submit.py
+++ submit.py
@@ -1,8 +1,4 @@
-### The submission script for PA3 of CS276, 2015
-
-### The only things you'll have to edit (unless you're porting this script over to a different language) 
-### are at the bottom of this file.
-
+import os
 import urllib
 import urllib2
 import hashlib
@@ -12,60 +8,99 @@ import email.message
 import email.encoders
 import StringIO
 import sys
+import json
+import subprocess
+import getpass
+from tempfile import NamedTemporaryFile
+from time import time
 from subprocess import Popen
 from subprocess import PIPE
-import subprocess
-from time import time
-from tempfile import NamedTemporaryFile
-import os
-import glob
-import zipfile
-import json
-import xmlrpclib 
-from urllib2 import urlopen
-import re
-import shutil
-import tempfile
 import datetime
-
-""""""""""""""""""""
-""""""""""""""""""""
 
 class NullDevice:
   def write(self, s):
     pass
 
-def submit():   
-  print '==\n== Submitting Solutions \n=='
-  
+def submit(partId):
+    
+  print '==\n== [inforetrieval] Submitting Solutions | Programming Exercise %s\n=='% homework_id()
+  if(not partId):
+    partId = promptPart()
+
+  partNames = validParts()
+  if not isValidPartId(partId):
+    print '!! Invalid homework part selected.'
+    print '!! Expected an integer from 0 to %d.' % (len(partNames) + 1)
+    print '!! Submission Cancelled'
+    return
+
   (login, password) = loginPrompt()
   if not login:
     print '!! Submission Cancelled'
     return
-  
-  print '\n== Connecting to Coursera ... '
 
-  # Part Identifier
-  (partIdx, sid) = partPrompt()
+  print '\n== Connecting to coursera ... '
+
+  # Setup submit list
+  if partId == len(partNames):
+    submitParts = range(0, len(partNames))
+  else:
+    submitParts = [partId]
+  print submitParts
+  for partId in submitParts:
+    # Get Challenge
+    (login, ch, state, ch_aux) = getChallenge(login, partId)
+    if((not login) or (not ch) or (not state)):
+      # Some error occured, error string in first return element.
+      print '\n!! Error: %s\n' % login
+      return
+
+    # Get source files
+    src = source(partId)
+    
+
+    # Attempt Submission with Challenge
+    ch_resp = challengeResponse(login, password, ch)
+    (result, string) = submitSolution(login, ch_resp, partId, output(partId, ch_aux), \
+                                    src, state, ch_aux)
+    print '== [inforetrieval] Submitted Homework %s - Part %d - %s' % \
+          (homework_id(), partId, partNames[partId])
+    print '== %s' % string.strip()
+    if (string.strip() == 'Exception: We could not verify your username / password, please try again. (Note that your password is case-sensitive.)'):
+      print '== The password is not your login, but a 10 character alphanumeric string displayed on the top of the Assignments page'
+
+## This collects the source code (just for logging purposes) 
+def source(partId):
+  if partId == 0:
+    if not os.path.exists("report.pdf"):
+      print "No report.pdf file found in the directory. Please make sure it exists (and make sure it's all lowercase letters)."
+      sys.exit(1);
+
+    p = open("report.pdf","rb").read().encode("base64")
+    return p
+
+  return ""
+
+def promptPart():
+  """Prompt the user for which part to submit."""
+  print('== Select which part(s) to submit for assignment ' + homework_id())
+  partNames = validParts()
+  for i in range(0, len(partNames)):
+    print '==   %d) %s' % (i, partNames[i])
+  print '==   %d) All of the above \n==\n ' % (len(partNames))
+  selPart = raw_input('Enter your choice [0-{0}]: '.format(len(partNames)))
+  partId = int(selPart)
+  if not isValidPartId(partId):
+    partId = -1
+  return partId
+
+def isValidPartId(partId):
+  """Returns true if partId references a valid part."""
+  partNames = validParts()
+  return ((partId >= 0) and (partId <= len(partNames) + 1))
 
 
-  # Get Challenge
-  (login, ch, state, ch_aux) = getChallenge(login, sid) #sid is the "part identifier"
-
-  if((not login) or (not ch) or (not state)):
-    # Some error occured, error string in first return element.
-    print '\n!! Error: %s\n' % login
-    return
-
-  # Attempt Submission with Challenge
-  ch_resp = challengeResponse(login, password, ch)
-  (result, string) = submitSolution(login, ch_resp, sid, output(partIdx,ch_aux), \
-                                  source(partIdx), state, ch_aux)
-
-  print '== %s' % string.strip()
-
-
-# =========================== LOGIN HELPERS - NO NEED TO CONFIGURE THIS =======================================
+# =========================== LOGIN HELPERS ===========================
 
 def loginPrompt():
   """Prompt the user for login credentials. Returns a tuple (login, password)."""
@@ -76,34 +111,36 @@ def loginPrompt():
 def basicPrompt():
   """Prompt the user for login credentials. Returns a tuple (login, password)."""
   login = raw_input('Login (Email address): ')
-  password = raw_input('One-time Password (from the assignment page. This is NOT your own account\'s password): ')
+  password = raw_input('Password: ')
   return login, password
 
-def partPrompt():
-  print 'Hello! These are the assignment parts that you can submit:'
-  counter = 0
-  partFriendlyNames = validParts()
-  for part in partFriendlyNames:
-    counter += 1
-    print str(counter) + ') ' + partFriendlyNames[counter - 1]
-  partIdx = int(raw_input('Please enter which part you want to submit (1-' + str(counter) + '): ')) - 1
-  return (partIdx, partIds[partIdx])
 
-def getChallenge(email, sid):
+def homework_id():
+  """Returns the string homework id."""
+  return '4'
+
+
+def getChallenge(email, partId):
   """Gets the challenge salt from the server. Returns (email,ch,state,ch_aux)."""
   url = challenge_url()
-  values = {'email_address' : email, 'assignment_part_sid' : sid, 'response_encoding' : 'delim'}
+  values = {'email_address' : email, 'assignment_part_sid' : "%s-%s" % (homework_id(), partId), 'response_encoding' : 'delim'}
+  # values = {'email_address' : email, 'assignment_part_sid' : "%s-%s-dev" % (homework_id(), partId), 'response_encoding' : 'delim'}
   data = urllib.urlencode(values)
   req = urllib2.Request(url, data)
   response = urllib2.urlopen(req)
   text = response.read().strip()
-
+ 
+#  print text
+#  print values
+#  sys.exit(1)
   # text is of the form email|ch|signature
   splits = text.split('|')
   if(len(splits) != 9):
     print 'Badly formatted challenge response: %s' % text
     return None
   return (splits[2], splits[4], splits[6], splits[8])
+
+
 
 def challengeResponse(email, passwd, challenge):
   sha1 = hashlib.sha1()
@@ -112,89 +149,86 @@ def challengeResponse(email, passwd, challenge):
   strAnswer = ''
   for i in range(0, len(digest)):
     strAnswer = strAnswer + digest[i]
-  return strAnswer 
-  
+  return strAnswer
+
+
 def challenge_url():
   """Returns the challenge url."""
   return "https://stanford.coursera.org/" + URL + "/assignment/challenge"
-
+  #return "https://stanford.coursera.org/inforetrieval/assignment/challenge"
+  #return "https://stanford.coursera.org/inforetrieval-staging/assignment/challenge"
+  
 def submit_url():
   """Returns the submission url."""
   return "https://stanford.coursera.org/" + URL + "/assignment/submit"
-
-def submitSolution(email_address, ch_resp, sid, output, source, state, ch_aux):
+  return "https://stanford.coursera.org/inforetrieval/assignment/submit"
+  #return "https://stanford.coursera.org/inforetrieval-staging/assignment/submit"
+  
+def submitSolution(email_address, ch_resp, part, output, source, state, ch_aux):
   """Submits a solution to the server. Returns (result, string)."""
+  #source_64_msg = email.message.Message()
+  #source_64_msg.set_payload(source)
+  #email.encoders.encode_base64(source_64_msg)
+
   output_64_msg = email.message.Message()
   output_64_msg.set_payload(output)
   email.encoders.encode_base64(output_64_msg)
-  values = { 'assignment_part_sid' : sid, \
+  values = { 'assignment_part_sid' : ("%s-%s" % (homework_id(), part)), \
+  # values = { 'assignment_part_sid' : ("%s-%s-dev" % (homework_id(), part)), \
              'email_address' : email_address, \
              'submission' : output_64_msg.get_payload(), \
              'submission_aux' : source, \
              'challenge_response' : ch_resp, \
              'state' : state \
            }
-  url = submit_url()  
+  url = submit_url()
+  inp = raw_input('Do you want to actually submit this (yes|y)?: ') #CHANGELIVE
+  inp = inp.strip().lower()
+  if inp != 'y' and inp != 'yes':
+    print '== Fine, aborting'
+    sys.exit(0) #CHANGELIVE
   data = urllib.urlencode(values)
   req = urllib2.Request(url, data)
   response = urllib2.urlopen(req)
   string = response.read().strip()
+  # print "STRING: " + string
+  # TODO parse string for success / failure
   result = 0
   return result, string
 
-
-## This collects the source code (just for logging purposes) 
-def source(partIdx):
-  if partIdx == 0:
-      
-      if not os.path.exists("report.pdf"):
-          print "No report.pdf file found in the directory. Please make sure it exists (and make sure it's all lowercase letters)."
-          sys.exit(1);
-          
-      p = open("report.pdf","rb").read().encode("base64")
-      return p
-  
-  return ""
-
-############ BEGIN ASSIGNMENT SPECIFIC CODE - YOU'LL HAVE TO EDIT THIS ##############
-
-# Make sure you change this string to the last segment of your class URL.
+############ BEGIN ASSIGNMENT SPECIFIC CODE ##############
 # For example, if your URL is https://class.coursera.org/pgm-2012-001-staging, set it to "pgm-2012-001-staging".
 URL = 'cs276-003'
-linesOutput = 1482
 
-# the "Identifier" you used when creating the part
-partIds = ['pa3_report','pa3_task1','pa3_task2','pa3_task3', 'pa3_task4']          
-# used to generate readable run-time information for students
 def validParts():
   """Returns a list of valid part names."""
   partNames = ['Report', \
-               'Task 1 (Cosine Similarity)', \
-               'Task 2 (BM-25)', \
-               'Task 3 (Smallest Window)', \
+               'Task 1 (Linear Regression)', \
+               'Task 2 (Ranking SVM)', \
+               'Task 3 (More Features)', \
                'Task 4 (Extra Credit)']
   return partNames
-    
-def ensure_dir(d):
-    if not os.path.exists(d):
-        os.makedirs(d)
-        
-def output(partIdx,ch_aux):
-  print '== Running your code ...'
-  print '== Your code should output results (and nothing else) to stdout'
-  # tempoutfile = tempfile.mkdtemp()
-  # getFiles(tempoutfile)
-  outputString = ''
 
-  tempoutfile = '.tmp' + str(random.randint(0,1<<20))
-  localfile = open(tempoutfile,'w')
-  localfile.write(ch_aux)
-  localfile.close()
-  
+# Thang: factor code into a new function to run different tasks
+def run_task(task_id, test_file, test_output_length):
+  start = time()
+  child = Popen(['./l2r.sh', 'data/pa4.signal.train', 'data/pa4.rel.train', test_file, task_id], stdout=PIPE, stderr=PIPE, shell=False);
+  (res, err) = child.communicate("")
+  # print res
+  elapsed = time() - start
+  guesses = res.splitlines()
+  print err
+  if (len(guesses) != test_output_length):
+      print 'Warning. The number of url-document pairs ' + str(len(guesses)) + ' is not correct. Please ensure that the output is formatted properly.'
+      print guesses
+  return (res, elapsed)
+
+def output(partId, ch_aux):
+  """Uses the student code to compute the output for test cases."""
   if not os.path.exists("people.txt"):
       print "There is no people.txt file in this directory. Please make people.txt file in this directory with your and your partner's SUNet ID in separate lines (do NOT include @stanford.edu)"
       sys.exit(1)
-  
+
   people = open("people.txt").read().splitlines();
   if len(people) == 0:
       print "people.txt is empty! Write the SUNet ids of you and your partner, if any, in separate lines.."
@@ -204,88 +238,51 @@ def output(partIdx,ch_aux):
         print "The SUNet IDs don't seem to be correct. Make sure to remove empty lines. They are supposed to be between 3 and 8 characters. Also, make sure to not include @stanford.edu."
         sys.exit(1);
   peopleStr = "_".join(str(x.strip()) for x in people  if x)
-  stats = {}
-  stats['USERIDS']= peopleStr  
-  stats['TIMESUBMITTED'] = str(datetime.datetime.now());
-  
+ 
   elapsed= 0.0 
-  if partIdx == 0:
+  dev_elapsed = 0.0
+  results = []
+  dev_results = []
+  if partId == 0:
     print 'Submitting the report'
+  elif partId <= 4 and partId>0:
+    print '== Make sure data/pa4.signal.(train|dev) and data/pa4.rel.(train|dev) are in the current working directory'
+    print '== Running your code ...'
+    print '== Your code should output results (and nothing else) to stdout'
 
-  elif partIdx == 1:
-    print '== Calling ./rank.sh for Task 1 (this might take a while)'
-    start = time()
-    child = Popen(['./rank.sh', tempoutfile,'cosine'], stdout=PIPE, stderr=PIPE, shell=False)
-    (res, err) = child.communicate("")
-    elapsed = time() - start
-    guesses = res.splitlines()
-    print '== Your stdout output is:'
-    print res
-    print '== Your stderr output is:'
-    print err
-    if (len(guesses) != linesOutput):
-        print '== Warning. The number of url-document pairs ' + str(len(guesses)) + ' is not correct. Please ensure that the output is formatted properly.'
-    stats['result'] = res
+    print '## Calling ./l2r.sh for Task', str(partId), '(this might take a while)'
+    
+    # dev
+    dev_file = 'data/pa4.signal.dev'
+    print '# Running on development test data ...'
+    dev_output_length = 1482
+    (dev_results, dev_elapsed) = run_task(str(partId), dev_file, dev_output_length)
 
-  elif partIdx == 2:
-      print '== Calling ./rank.sh for Task 2 (this might take a while)'
-      start = time()
-      child = Popen(['./rank.sh', tempoutfile,'bm25'], stdout=PIPE, stderr=PIPE, shell=False)
-      (res, err) = child.communicate("")
-      elapsed = time() - start
-      guesses = res.splitlines()
-      print '== Your stdout output is:'
-      print res
-      print '== Your stderr output is:'
-      print err
-      if (len(guesses) != linesOutput):
-          print '== Warning. The number of url-document pairs is not correct. Please ensure that the output is formatted properly.'
-      stats['result'] = res
-
-  elif partIdx == 3:
-      print '== Calling ./rank.sh for Task 3 (this might take a while)'
-      start = time()
-      child = Popen(['./rank.sh',tempoutfile,'window'], stdout=PIPE, stderr=PIPE, shell=False)
-      (res, err) = child.communicate("")
-      elapsed = time() - start
-      guesses = res.splitlines()
-      print '== Your stdout output is:'
-      print res
-      print '== Your stderr output is:'
-      print err
-      if (len(guesses) != linesOutput):
-          print '== Warning. The number of url-document pairs is not correct. Please ensure that the output is formatted properly.'
-      stats['result'] = res
-
-  elif partIdx == 4:
-      print '== Calling ./rank.sh for Task 4 (this might take a while)'
-      start = time()
-      child = Popen(['./rank.sh', tempoutfile,'extra'], stdout=PIPE, stderr=PIPE, shell=False)
-      (res, err) = child.communicate("")
-      elapsed = time() - start
-      guesses = res.splitlines()
-      print '== Your stdout output is:'
-      print res
-      print '== Your stderr output is:'
-      print err
-      if (len(guesses) != linesOutput):
-          print '== Warning. The number of url-document pairs is not correct. Please ensure that the output is formatted properly.'
-      stats['result'] = res
-      
+    # test
+    print '# Running on test data ...'
+    test_file = NamedTemporaryFile(delete=False)
+    test_file.write(ch_aux)
+    test_file.close()
+    test_file = test_file.name
+    test_output_length = 1482
+    (results, elapsed) = run_task(str(partId), test_file, test_output_length)
   else:
-    print '[WARNING]\t[output]\tunknown partId: %s' % partIdx
+    print '[WARNING]\t[output]\tunknown partId: %s' % partId
     sys.exit(1)
-
   print '== Finished running your code'
-  os.remove(tempoutfile)
 
-  return json.dumps(stats).strip()
+  # print dev_results
+
+  # print results
+  
+  return json.dumps( { 'dev_result': dev_results, 'dev_time': dev_elapsed, 'result': results, 'time': elapsed, 'timesubmitted': str(datetime.datetime.now()), 'USERIDS': peopleStr } )
 
 def test_python_version():
-    """docstring for test_python_version"""
-    if sys.version_info < (2,6):
-        print >> sys.stderr, "Your python version is too old, please use >= 2.6"
-        sys.exit(1)
+  """docstring for test_python_version"""
+  if sys.version_info < (2,6):
+    print >> sys.stderr, "Your python version is too old, please use >= 2.6"
+    sys.exit(1)
 
-test_python_version()
-submit()
+if __name__ == '__main__':
+  test_python_version()
+  submit(0)
